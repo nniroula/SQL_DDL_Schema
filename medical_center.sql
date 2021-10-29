68b4f7fd2fa658c79bfccb9110026b9f88575ce2
nabin <nkbhaie2017@gmail.com>
2021-10-29 16:35:21 -0600

    add a table

---
+++ medical_center.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS medical_record;
+CREATE DATABASE medical_record;
+\c medical_record;
+
+CREATE TABLE medical_center(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+INSERT INTO medical_center(id, name)
+VALUES (1,  'University Hospital');
+
+INSERT INTO medical_center(id, name)
+VALUES (2,  'Aurora Medical Center');
