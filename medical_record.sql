52e5823aa4591e278c2a785714e6383ebe9e6b01
nabin <nkbhaie2017@gmail.com>
2021-10-29 17:32:38 -0600

    create more tables

--- medical_record.sql
+++ medical_record.sql
@@ -31,3 +31,26 @@ INSERT INTO doctors(id, name, medical_center_id)
 VALUES(1, 'Dr. Prinsha', 1),
     (2, 'Dr. Prabha', 1),
     (3, 'Dr. Jones', 3);
+
+-- create patients table
+CREATE TABLE patients(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    doctor_id INTEGER REFERENCES doctors(id)
+);
+
+INSERT INTO patients(id, name, doctor_id)
+VALUES(1, 'Cristy', 1),
+        (2, 'Kevin', 2),
+        (3, 'Nabin', 1);
+
+-- Create dieseas table
+CREATE TABLE diseases(
+    id SERIAL PRIMARY KEY,
+    disease TEXT NOT NULL,
+    patient_id INTEGER REFERENCES patients(id)
+);
+
+INSERT INTO diseases(id, disease, patient_id)
+VALUES(1, 'Dark Circle', 3);
+
