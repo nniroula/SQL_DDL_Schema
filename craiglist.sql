97a7a30f27a6d83c2b00040bbf9f7e5196958cdc
nabin <nkbhaie2017@gmail.com>
2021-10-29 18:17:07 -0600

    inset values into tbales

--- craiglist.sql
+++ craiglist.sql
@@ -8,13 +8,19 @@ CREATE TABLE lists(
 );
 INSERT INTO lists(id, region)
 VALUES(1, 'DENVER'),
-    (2, 'Colodao Springs')
+    (2, 'Colodao Springs'),
     (3, 'Aurora');
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     preferred_region_id INTEGER REFERENCES lists(id)
 );
+INSERT INTO users(id, name, preferred_region_id)
+VALUES(1, 'John', 1),
+    (2, 'Alex', 1),
+    (3, 'Husky', 2),
+    (4, 'Tiffany', 3);
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
